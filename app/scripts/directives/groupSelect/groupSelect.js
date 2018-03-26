e77c1ba782c92cb7787815da28d549086a887d6d
wuxiang <303152175@qq.com>
2018-03-26 15:19:21 +0800

    0326

--- app/scripts/directives/groupSelect/groupSelect.js
+++ app/scripts/directives/groupSelect/groupSelect.js
@@ -111,6 +111,9 @@ angular.module('sbAdminApp')
                 if(item.id != $scope.currentLeaf.id){
                     $scope.currentLeaf = item;
                     $scope.$emit('emitGroupLeaf', $scope.currentGroup, $scope.currentLeaf);
+                    // setTimeout(function(){  
+                    //     $scope.$apply();//必需手动进行脏值检测,否则数据无法刷新到界面  
+                    // },1); 
                 }
             }
             $scope.showLeafMenu = function ($event) {
