78d529f59dd0c6549d9f244f3790400642ba5acc
wuxiang <303152175@qq.com>
2018-06-12 19:49:00 +0800

    0612

--- app/scripts/modules/home/index.js
+++ app/scripts/modules/home/index.js
@@ -14,8 +14,28 @@ angular.module('sbAdminApp', [])
 		$scope.ter_offline = 0;
 
 		$scope.ter_error = 0;
+		$scope.materialReport = [];
 		baseService.getJson(baseService.api.apiUrl + 'api/auth/getMainPageReport',{},function(res){
-			console.log(res)
+			for(var j = 0; j < res.materialReport.length; j ++){
+				var item = res.materialReport[j];
+				if(item.type == 1){
+					item.name = '视频';
+					item.color = '#00a0e9';
+					$scope.materialReport.unshift(item);
+				}else if(item.type == 0){
+					item.name = '图片';
+					item.color = '#faae30';
+					$scope.materialReport.push(item);
+				}else if(item.type == 2){
+					item.name = '音乐';
+					item.color = '#7fbe25';
+					$scope.materialReport.push(item);
+				}else if(item.type == 3){
+					item.name = '互动包';
+					item.color = '#ff4040';
+					$scope.materialReport.push(item);
+				}
+			}
 			for(var i = 0; i < res.terminalReport.length; i ++){
 				if(res.terminalReport[i].status == 1){
 					$scope.ter_ok = res.terminalReport[i].allCount;
@@ -29,5 +49,11 @@ angular.module('sbAdminApp', [])
 				$scope.ter_all = $scope.ter_ok + $scope.ter_offline + $scope.ter_error + $scope.ter_noAct;
 			}
 		})
+		$scope.materialStyle = function(color){
+			return {
+				color: color,
+				border: '1px solid ' +  color
+			}
+		}
 	}]);
 // JavaScript Document
