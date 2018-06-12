ca5bba91c6f5472b14b12ac33c3582952db3d5a6
wuxiang <303152175@qq.com>
2018-06-12 16:15:42 +0800

    0612

--- app/scripts/modules/home/index.js
+++ app/scripts/modules/home/index.js
@@ -8,6 +8,11 @@
  */
 angular.module('sbAdminApp', [])
 	.controller('homeCtrl', ['$scope', '$window', '$rootScope', 'baseService', function ($scope, $window, $rootScope, baseService) {
-		
+		$scope.ter_all = 0;
+		$scope.ter_ok = 0;
+		$scope.ter_noAct = 0;
+		$scope.ter_offline = 0;
+
+		$scope.ter_error = 0;
 	}]);
 // JavaScript Document
